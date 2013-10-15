8862c424471ce858655a53e654eb97cab7d0068c
森山 雄貴 <moriyamayuuki@moriyama-yuki-no-MacBook-Pro.local>
2013-10-16 00:25:58 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MissFacebook::Application.config.secret_key_base = 'e787a677591dce8ade1ea0e15109f96e86753b42bf2cf52a3493e2dc8c6630f0447a02b18e5975c14a9b13b56d480cba0b8d57993281bc6e44fbdb9d5a4e2f10'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MissFacebook::Application.config.session_store :cookie_store, key: '_miss-facebook_session'
