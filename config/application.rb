975d6e974118071a0fdd085a05bace41f4fbf3a1
Yuki Moriyama <yuki.moriyaman@gmail.com>
2013-10-16 02:50:53 +0900

    Gemの変更

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,27 @@ module MissFacebook
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    
+    # Set timezone
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
+    # 日本語化
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    # generatorの設定
+    config.generators do |g|
+      g.orm :active_record
+      g.template_engine :slim
+      #g.test_framework  :rspec, :fixture => true
+      #g.fixture_replacement :factory_girl, :dir => "spec/factories"
+      g.view_specs false
+      g.helper_specs false
+    end
+
+    # libファイルの自動読み込み
+    config.autoload_paths += %W(#{config.root}/lib)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"] 
   end
 end
