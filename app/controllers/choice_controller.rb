88fe2a00d6723cfa40889eb2e58af943a6f621a3
Yuki Moriyama <yuki.moriyaman@gmail.com>
2013-10-17 11:18:29 +0900

    画像修正

--- app/controllers/choice_controller.rb
+++ app/controllers/choice_controller.rb
@@ -2,6 +2,6 @@ class ChoiceController < ApplicationController
 
   def index
     @photo_1 = UserPhoto.where(id: 9).first
-    @photo_2 = UserPhoto.where(id: 8).last
+    @photo_2 = UserPhoto.where(id: 11).last
   end
 end
--- app/models/user_photo.rb
+++ app/models/user_photo.rb
@@ -6,13 +6,8 @@ class UserPhoto < ActiveRecord::Base
  
   has_attached_file :avatar, {
     :styles => {
-      :thumb => ["50x50#", :png],
-      :medium => ["100x100#", :png],
-      :large => ["300x300>", :png]
-    },
-    :convert_options => {
-      :thumb => "-gravity Center -crop 50x50+0+0",
-      :thumb => "-gravity Center -crop 100x100+0+0",
+       :medium => ['100x100#', :png], 
+       :large => ['1000x1000>', :png]
     }
   }
 
