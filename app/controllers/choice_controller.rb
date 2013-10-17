9a2b39a1fbacc00477a70fee881bd4c64d434dfd
masamitsu-konya <masamitsu.konya@gmail.com>
2013-10-18 00:55:25 +0900

    プロフィールのチェックボックス習性

--- app/controllers/choice_controller.rb
+++ app/controllers/choice_controller.rb
@@ -1,7 +1,8 @@
 class ChoiceController < ApplicationController
 
   def index
-    @stay_user = @login_user.user_likes.exists? ? @login_user.user_likes.last.user : User.women.has_photo.sort_by{rand}.first
+    #@stay_user = @login_user.user_likes.exists? ? @login_user.user_likes.last.user : User.women.has_photo.sort_by{rand}.first
+    @stay_user = User.women.has_photo.sort_by{rand}.last
     @new_user = User.women.has_photo.sort_by{rand}.last
     @stay_user.score >= @new_user.score ? @stay_user.to_beyond_user : @new_user.to_beyond_user
   end
